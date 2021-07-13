17ed93edffc879dbbb20b7dc12b17d85151ef265
shj20000107 <haijian_shen@163.com>
2021-07-14 03:24:39 +0800

    购物车页面彻底完成，初步完成确认订单页面
    确认订单页面的前端工作已经完成，还没有写数据库；此外，cart数据传递传的是整个cart，还没有筛选出checked；生成、取消订单的业务逻辑还需商榷。

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -11,7 +11,7 @@ Page({
     isSelectAddress: false,
     address:[],
     cart: [],
-    allChecked: false,
+    allChecked: true,
     totalPrice: 0,
     totalNum: 0
   },
@@ -21,9 +21,53 @@ Page({
     })
   },
   onShow: function () {
+    //接收来自address_sel页面的地址数据
+    this.getAddress();
+    // 获取数据库中cart的数据,并计算底部条
+    let that = this
+    wx.cloud.init()
+    wx.cloud.database().collection("cart").where({
+      _openid:that.data.openid
+    }).get({
+      success(res) {
+        console.log("请求成功", res)
+        that.setData({
+          cart: res.data
+        })
+        // 确定allChecked
+        let allChecked = true;
+        console.log(1);
+        // 确定total
+        let totalPrice = 0; 
+        let totalNum = 0;
+        console.log(2);
+        for(let i = 0; i < that.data.cart.length; i++){
+          console.log(3);
+          if(that.data.cart[i].checked){
+            totalPrice+=that.data.cart[i].number*that.data.cart[i].book_price;
+            totalNum+=that.data.cart[i].number;
+          }
+          else{
+            allChecked = false;
+          }
+        }
+        console.log(totalPrice);
+        console.log(4);
+        that.setData({
+          allChecked,
+          totalPrice,
+          totalNum
+        })
+      },
+      fail(res){
+        console.log("请求失败", res)
+      }
+    })
+  },
+  // 获取地址功能
+  async getAddress(){
     var pages = getCurrentPages();
     var currPage = pages[pages.length - 1]; //当前页面
-    //接收来自address_sel页面的地址数据
     this.setData({
       address: currPage.__data__.address,
     })
@@ -37,87 +81,261 @@ Page({
         isSelectAddress: false
       })
     }
-    // 获取数据库中cart的数据
-    let that = this
-    wx.cloud.init()
-    wx.cloud.database().collection("cart").where({
-       _openid:that.data.openid
-    }).get({
-      success(res) {
-        console.log("请求成功", res)
-        that.setData({
-          cart: res.data
-        })
-      },
-      fail(res){
-        console.log("请求失败", res)
-      }
+  },
+  // 选中商品功能
+  handleItemChange(e){
+    const id = e.currentTarget.dataset.id;
+    console.log(id);
+    let {cart} = this.data;
+    let index = cart.findIndex(v=>v._id===id);
+    cart[index].checked = !cart[index].checked;
+    this.setData({
+      cart
     })
     // 确定allChecked
     const allChecked = this.data.cart.every(v=>v.checked);
+    console.log(1);
     // 确定total
     let totalPrice = 0; 
     let totalNum = 0;
+    console.log(2);
     for(let i = 0; i < this.data.cart.length; i++){
       if(this.data.cart[i].checked){
+        console.log(3);
         totalPrice+=this.data.cart[i].number*this.data.cart[i].book_price;
         totalNum+=this.data.cart[i].number;
-        console.log(totalNum);
-        console.log(totalPrice);
       }
     }
+    console.log(4);
     this.setData({
       allChecked,
       totalPrice,
       totalNum
-    })
-  },
-  handleItemChange(e){
-    const cart_id = e.currentTarget.dataset.id;
-    console.log(cart_id);
-    let {cart} = this.data;
-    let index = cart.findIndex(v=>v._id===cart_id);
-    cart[index].checked = !cart[index].checked;
-    this.setData({
-      cart
     })
     let that = this
     wx.cloud.init()
     var db = wx.cloud.database()
     var  _ = db.command
     db.collection("cart").where({
-      _id: cart_id
+      _id: id
     }).get({
       success:(res)=>{
         console.log("数据请求成功",res)
         db.collection("cart").doc(res.data[0]._id).update({
           data:{
-            checked: this.data.cart[index].checked
+            checked: that.data.cart[index].checked
           }
         })
       }
     })
+  },
+  // 全选 反选 功能
+  handleItemAllChecked(e){
+    let {cart, allChecked} = this.data;
+    allChecked = !allChecked;
+    cart.forEach(v=>v.checked = allChecked);
+    this.setData({
+      cart
+    })
     // 确定allChecked
-    const allChecked = this.data.cart.every(v=>v.checked);
+    allChecked = this.data.cart.every(v=>v.checked);
+    console.log(1);
     // 确定total
     let totalPrice = 0; 
     let totalNum = 0;
+    console.log(2);
     for(let i = 0; i < this.data.cart.length; i++){
       if(this.data.cart[i].checked){
+        console.log(3);
         totalPrice+=this.data.cart[i].number*this.data.cart[i].book_price;
         totalNum+=this.data.cart[i].number;
-        console.log(totalNum);
-        console.log(totalPrice);
       }
     }
+    console.log(4);
     this.setData({
       allChecked,
       totalPrice,
       totalNum
     })
+    let that = this
+    wx.cloud.init()
+    var db = wx.cloud.database()
+    var  _ = db.command
+    db.collection("cart").where({
+      _openid:that.data.openid
+    }).update({
+      data:{
+        checked: allChecked
+      }
+    })
   },
+  // 改变商品个数
+  async handleItemNumEdit(e){
+    const {operation, id} = e.currentTarget.dataset;
+    console.log(operation,id);
+    let {cart} = this.data;
+    const index = cart.findIndex(v=>v._id===id);
+    cart[index].number += Number(operation);
+    if(cart[index].number === 0){
+      wx.showModal({
+        title: '提示',
+        content: '是否删除该商品',
+        success :(res)=>{
+          if (res.confirm) {
+            cart.splice(index,1);
+            this.setData({
+              cart
+            })
+            // 确定allChecked
+            const allChecked = this.data.cart.every(v=>v.checked);
+            console.log(1);
+            // 确定total
+            let totalPrice = 0; 
+            let totalNum = 0;
+            console.log(2);
+            for(let i = 0; i < this.data.cart.length; i++){
+              if(this.data.cart[i].checked){
+                console.log(3);
+                totalPrice+=this.data.cart[i].number*this.data.cart[i].book_price;
+                totalNum+=this.data.cart[i].number;
+              }
+            }
+            console.log(4);
+            this.setData({
+              allChecked,
+              totalPrice,
+              totalNum
+            })
+            let that = this
+            wx.cloud.init()
+            var db = wx.cloud.database()
+            var  _ = db.command
+            db.collection("cart").where({
+              _id: id
+            }).get({
+              success:(res)=>{
+                console.log("数据删除成功",res)
+                db.collection("cart").doc(res.data[0]._id).remove({
+                  success: function(res) {
+                    that.onShow()
+                  }
+                })
+              }
+            })
+          } else if (res.cancel) {
+            cart[index].number = 1;
+            this.setData({
+              cart
+            })
+            // 确定allChecked
+            const allChecked = this.data.cart.every(v=>v.checked);
+            console.log(1);
+            // 确定total
+            let totalPrice = 0; 
+            let totalNum = 0;
+            console.log(2);
+            for(let i = 0; i < this.data.cart.length; i++){
+              if(this.data.cart[i].checked){
+                console.log(3);
+                totalPrice+=this.data.cart[i].number*this.data.cart[i].book_price;
+                totalNum+=this.data.cart[i].number;
+              }
+            }
+            console.log(4);
+            this.setData({
+              allChecked,
+              totalPrice,
+              totalNum
+            })
+            let that = this
+            wx.cloud.init()
+            var db = wx.cloud.database()
+            var  _ = db.command
+            db.collection("cart").where({
+              _id: id
+            }).get({
+              success:(res)=>{
+                console.log("数据修改成功",res)
+                db.collection("cart").doc(res.data[0]._id).update({
+                  data:{
+                    number: that.data.cart[index].number
+                  }
+                })
+              }
+            })
+          }
+        }
+      })
+    }
+    else{
+      this.setData({
+        cart
+      })
+      // 确定allChecked
+      const allChecked = this.data.cart.every(v=>v.checked);
+      console.log(1);
+      // 确定total
+      let totalPrice = 0; 
+      let totalNum = 0;
+      console.log(2);
+      for(let i = 0; i < this.data.cart.length; i++){
+        if(this.data.cart[i].checked){
+          console.log(3);
+          totalPrice+=this.data.cart[i].number*this.data.cart[i].book_price;
+          totalNum+=this.data.cart[i].number;
+        }
+      }
+      console.log(4);
+      this.setData({
+        allChecked,
+        totalPrice,
+        totalNum
+      })
+      let that = this
+      wx.cloud.init()
+      var db = wx.cloud.database()
+      var  _ = db.command
+      db.collection("cart").where({
+        _id: id
+      }).get({
+        success:(res)=>{
+          console.log("数据修改成功",res)
+          db.collection("cart").doc(res.data[0]._id).update({
+            data:{
+              number: that.data.cart[index].number
+            }
+          })
+        }
+      })
+    }
+  },
+  // 结算功能
+  async handlePay(e){
+    const {address, totalNum} = this.data;
+    if(!address.name){
+      wx.showToast({
+        title: '请选择地址',
+        icon: 'none'
+      })
+      return;
+    }
+    if(totalNum === 0){
+      wx.showToast({
+        title: '请选购商品',
+        icon: 'none'
+      })
+      return;
+    }
+    var n_address = JSON.stringify(this.data.address);
+    var cart = JSON.stringify(this.data.cart);
+    var n_totalNum = String(this.data.totalNum);
+    var totalPrice = String(this.data.totalPrice);
+    wx.navigateTo({
+      url: '/pages/order_submit/order_submit?address=' + n_address + '&cart=' + cart + '&totalNum=' + n_totalNum + '&totalPrice=' + totalPrice
+    })
+  },
+  //获取地址按钮事件
   handleChooseAddress(){
-    console.log("点了");
     wx.navigateTo({
       url: '/pages/address_sel/address_sel',
     })
